c109b5171230e29fc22630f0e22063cb1e8bb528
Timilehin Arigbede <harriemannie@gmail.com>
2022-10-26 18:49:47 +0100

    Update User Functionality

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,10 @@
-const express = require('express')
-const router = express.Router()
-const { register, login } = require('../controllers/auth')
-router.post('/register', register)
-router.post('/login', login)
+const express = require("express");
 
-module.exports = router
+const router = express.Router();
+const authenticateUser = require("../middleware/authentication");
+const { register, login, updateUser } = require("../controllers/auth");
+router.post("/register", register);
+router.post("/login", login);
+router.patch("/updateUser", authenticateUser, updateUser);
+
+module.exports = router;
