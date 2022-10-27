ef7f32ee2273633bd5a76929250452a6c7b004a2
Timilehin Arigbede <harriemannie@gmail.com>
2022-10-27 10:06:09 +0100

    Pagination

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -3,7 +3,7 @@ const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, NotFoundError } = require("../errors");
 
 const getAllJobs = async (req, res) => {
-  const { status, jobType, sort, page, search } = req.query;
+  const { status, jobType, sort, search } = req.query;
 
   const queryObject = {
     createdBy: req.user.userId,
@@ -39,8 +39,19 @@ const getAllJobs = async (req, res) => {
     result = result.sort("-position");
   }
 
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const jobs = await result;
-  res.status(StatusCodes.OK).json({ jobs });
+
+  // set Pagination
+
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+  res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages });
 };
 const getJob = async (req, res) => {
   const {
